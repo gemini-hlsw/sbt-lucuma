6321b0f8615b628ca963a2665e2c5570255af1ae
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2020-09-14 14:13:16 -0300

    Add Todd and rename gsp to lucuma
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- src/main/scala/edu/gemini/gsp/sbtplugin/GspPlugin.scala
+++ src/main/scala/edu/gemini/gsp/sbtplugin/GspPlugin.scala
@@ -1,7 +1,7 @@
 // Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
 // For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
 
-package edu.gemini.gsp.sbtplugin
+package lucuma.sbtplugin
 
 import sbt._
 import sbt.Keys._
@@ -10,23 +10,23 @@ import de.heikoseeberger.sbtheader.HeaderPlugin
 import _root_.io.github.davidgregory084.TpolecatPlugin
 import scalafix.sbt.ScalafixPlugin
 
-object GspPlugin extends AutoPlugin {
+object LucumaPlugin extends AutoPlugin {
 
   import HeaderPlugin.autoImport._
   import ScalafixPlugin.autoImport._
 
   object autoImport {
 
-    lazy val gspGlobalSettings = Seq(
+    lazy val lucumaGlobalSettings = Seq(
       scalaVersion := "2.13.3",
       resolvers += Resolver.sonatypeRepo("public"),
       semanticdbEnabled := true, // enable SemanticDB
       semanticdbVersion := scalafixSemanticdb.revision, // use Scalafix compatible version
       scalafixDependencies += "com.github.liancheng" %% "organize-imports" % "0.4.0", // Include OrganizeImport scalafix
-      addCompilerPlugin(scalafixSemanticdb("4.3.20")) // This is needed for scalafix to run with scala 2.13.3
+      addCompilerPlugin(scalafixSemanticdb("4.3.22")) // This is needed for scalafix to run with scala 2.13.3
     )
 
-    lazy val gspHeaderSettings = Seq(
+    lazy val lucumaHeaderSettings = Seq(
       headerMappings := headerMappings.value + (HeaderFileType.scala -> HeaderCommentStyle.cppStyleLineComment),
       headerLicense  := Some(HeaderLicense.Custom(
         """|Copyright (c) 2016-2020 Association of Universities for Research in Astronomy, Inc. (AURA)
@@ -35,27 +35,28 @@ object GspPlugin extends AutoPlugin {
       ))
     )
 
-    lazy val gspPublishSettings = Seq(
+    lazy val lucumaPublishSettings = Seq(
       organization     := "edu.gemini",
       organizationName := "Association of Universities for Research in Astronomy, Inc. (AURA)",
       startYear        := Some(2019),
       licenses         += (("BSD-3-Clause", new URL("https://opensource.org/licenses/BSD-3-Clause"))),
       developers := List(
-        Developer("cquiroz",    "Carlos Quiroz",       "cquiroz@gemini.edu",    url("http://www.gemini.edu"  )),
-        Developer("jluhrs",     "Javier Lührs",        "jluhrs@gemini.edu",     url("http://www.gemini.edu"  )),
-        Developer("sraaphorst", "Sebastian Raaphorst", "sraaphorst@gemini.edu", url("http://www.gemini.edu"  )),
-        Developer("swalker2m",  "Shane Walker",        "swalker@gemini.edu",    url("http://www.gemini.edu"  )),
-        Developer("tpolecat",   "Rob Norris",          "rnorris@gemini.edu",    url("http://www.tpolecat.org")),
-        Developer("rpiaggio",   "Raúl Piaggio",        "rpiaggio@gemini.edu",   url("http://www.gemini.edu"  )),
+        Developer("cquiroz",      "Carlos Quiroz",       "cquiroz@gemini.edu",    url("http://www.gemini.edu"  )),
+        Developer("jluhrs",       "Javier Lührs",        "jluhrs@gemini.edu",     url("http://www.gemini.edu"  )),
+        Developer("sraaphorst",   "Sebastian Raaphorst", "sraaphorst@gemini.edu", url("http://www.gemini.edu"  )),
+        Developer("swalker2m",    "Shane Walker",        "swalker@gemini.edu",    url("http://www.gemini.edu"  )),
+        Developer("tpolecat",     "Rob Norris",          "rnorris@gemini.edu",    url("http://www.tpolecat.org")),
+        Developer("rpiaggio",     "Raúl Piaggio",        "rpiaggio@gemini.edu",   url("http://www.gemini.edu"  )),
+        Developer("toddburnside", "Todd Burnside",       "tburnside@gemini.edu",  url("http://www.gemini.edu"  )),
       )
     )
 
-    lazy val gspScalaJsSettings = Seq(
+    lazy val lucumaScalaJsSettings = Seq(
       scalacOptions ~= (_.filterNot(Set("-Xcheckinit")))
     )
 
-    lazy val gspCommonSettings =
-      gspHeaderSettings
+    lazy val lucumaCommonSettings =
+      lucumaHeaderSettings
 
   }
 
@@ -68,11 +69,11 @@ object GspPlugin extends AutoPlugin {
     allRequirements
 
   override val globalSettings =
-    gspGlobalSettings
+    lucumaGlobalSettings
 
   override val projectSettings =
-    inConfig(Compile)(gspCommonSettings) ++
-    inConfig(Test   )(gspCommonSettings)
+    inConfig(Compile)(lucumaCommonSettings) ++
+    inConfig(Test   )(lucumaCommonSettings)
 
 }
 
