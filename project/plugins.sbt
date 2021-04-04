2cac674ab6d83a19befeec3d76c0acd1057282f7
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2021-04-04 16:31:05 -0400

    Update sbt
    Signed-off-by: Carlos Quiroz <carlos.m.quiroz@gmail.com>

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,5 @@
 // Use this project as its own plugin
-unmanagedSourceDirectories in Compile += baseDirectory.value.getParentFile / "src" / "main" / "scala"
+Compile / unmanagedSourceDirectories += baseDirectory.value.getParentFile / "src" / "main" / "scala"
 
 addSbtPlugin("com.geirsson"              % "sbt-ci-release" % "1.5.7")
 addSbtPlugin("de.heikoseeberger"         % "sbt-header"     % "5.6.0")
--- src/main/scala/edu/gemini/gsp/sbtplugin/GspPlugin.scala
+++ src/main/scala/edu/gemini/gsp/sbtplugin/GspPlugin.scala
@@ -23,7 +23,7 @@ object LucumaPlugin extends AutoPlugin {
       semanticdbEnabled := true, // enable SemanticDB
       semanticdbVersion := scalafixSemanticdb.revision, // use Scalafix compatible version
       scalafixDependencies += "com.github.liancheng" %% "organize-imports" % "0.5.0", // Include OrganizeImport scalafix
-      addCompilerPlugin(scalafixSemanticdb("4.4.10")) // This is needed for scalafix to run with scala 2.13.4
+      addCompilerPlugin(scalafixSemanticdb("4.4.11")) // This is needed for scalafix to run with scala 2.13.5
     )
 
     lazy val lucumaHeaderSettings = Seq(
