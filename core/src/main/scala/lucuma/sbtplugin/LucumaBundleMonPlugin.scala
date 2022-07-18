5b7775ec4648f9477c28a546429384646b0a0461
Arman Bilge <armanbilge@gmail.com>
2022-07-18 21:45:33 +0000

    Automatically add bundleMon step to CI

---
+++ core/src/main/scala/lucuma/sbtplugin/LucumaBundleMonPlugin.scala
@@ -0,0 +1,26 @@
+// Copyright (c) 2016-2022 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
+package lucuma.sbtplugin
+
+import com.armanbilge.sbt.BundleMonPlugin
+import org.typelevel.sbt.gha.GenerativeKeys._
+import org.typelevel.sbt.gha.WorkflowStep
+import sbt._
+
+object LucumaBundleMonPlugin extends AutoPlugin {
+
+  override def trigger: PluginTrigger = allRequirements
+
+  override def requires: Plugins = LucumaPlugin && BundleMonPlugin
+
+  override def buildSettings: Seq[Setting[_]] = Seq(
+    ThisBuild / githubWorkflowBuild +=
+      WorkflowStep.Sbt(
+        List("bundleMon"),
+        name = Some("Monitor bundle size"),
+        cond = Some("matrix.project == 'rootJS'")
+      )
+  )
+
+}
